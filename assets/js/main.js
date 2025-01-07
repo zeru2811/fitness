a36fb2279dd44529ddc261ea2a3b04f22f3fe7d5
Z3ru2820 <waiyan.3l.2001@gmail.com>
2025-01-07 16:00:04 +0630

    add goals page

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,26 +327,22 @@
   /**
    * Initiate Datatables
    */
-  const datatables = select('.datatable', true)
-  datatables.forEach(datatable => {
-    new simpleDatatables.DataTable(datatable, {
-      perPageSelect: [5, 10, 15, ["All", -1]],
-      columns: [{
-          select: 2,
-          sortSequence: ["desc", "asc"]
-        },
-        {
-          select: 3,
-          sortSequence: ["desc"]
-        },
-        {
-          select: 4,
-          cellClass: "green",
-          headerClass: "red"
-        }
-      ]
-    });
-  })
+  const datatables = document.querySelectorAll('.datatable');
+datatables.forEach(datatable => {
+  new simpleDatatables.DataTable(datatable, {
+    perPageSelect: [5, 10, 15, ["All", -1]],
+    columns: [
+      {
+        select: 2,
+        sortSequence: ["desc", "asc"]
+      },
+      {
+        select: 3,
+        sortSequence: ["desc"]
+      }
+    ]
+  });
+});
 
   /**
    * Autoresize echart charts
@@ -408,3 +404,4 @@
 }).render();
 
 })();
+
